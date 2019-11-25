5391292eea69fedee4edff126b0b470b25319703
GfsTee <georg.treitz@gmail.com>
2019-11-25 19:26:39 +0100

    build added

---
+++ build2/precache-manifest.d1daa3a044eddef8a8c6ac03d1349ef9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920d7fa9b1cdc5022bd964ec4f38abb7",
+    "url": "/supercode-participants-react/index.html"
+  },
+  {
+    "revision": "56c31345ffcf1e74f113",
+    "url": "/supercode-participants-react/static/css/main.d7d15bfd.chunk.css"
+  },
+  {
+    "revision": "8dafbd4a790c31ae6a18",
+    "url": "/supercode-participants-react/static/js/2.56183a88.chunk.js"
+  },
+  {
+    "revision": "56c31345ffcf1e74f113",
+    "url": "/supercode-participants-react/static/js/main.d95cc013.chunk.js"
+  },
+  {
+    "revision": "e16e77f6ec5a74169a8b",
+    "url": "/supercode-participants-react/static/js/runtime-main.72a6f590.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/supercode-participants-react/static/media/logo.25bf045c.svg"
+  }
+]);
