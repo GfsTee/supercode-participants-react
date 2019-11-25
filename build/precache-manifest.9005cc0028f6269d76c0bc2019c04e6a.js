cee08fc06948d4faf34e57f50bebec4a9d9eadb0
GfsTee <georg.treitz@gmail.com>
2019-11-25 19:53:52 +0100

    git changes

---
+++ build/precache-manifest.9005cc0028f6269d76c0bc2019c04e6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1206bdb8781de6122ff85cd9abae1e98",
+    "url": "/supercode-participants-react/build/index.html"
+  },
+  {
+    "revision": "235e71b2b0a602a7d252",
+    "url": "/supercode-participants-react/build/static/css/main.6bb067a0.chunk.css"
+  },
+  {
+    "revision": "8dafbd4a790c31ae6a18",
+    "url": "/supercode-participants-react/build/static/js/2.56183a88.chunk.js"
+  },
+  {
+    "revision": "235e71b2b0a602a7d252",
+    "url": "/supercode-participants-react/build/static/js/main.4afb82d9.chunk.js"
+  },
+  {
+    "revision": "1e71eef95aa27657b116",
+    "url": "/supercode-participants-react/build/static/js/runtime-main.f45a6688.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/supercode/teilnehmer/precache-manifest.861a484ebfa5b42b4d68a58636b26561.js"
+  "/supercode-participants-react/build/precache-manifest.9005cc0028f6269d76c0bc2019c04e6a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/supercode/teilnehmer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/supercode-participants-react/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
